e2a33b59955a8cabf8d1777b0e7b8547624e3240
aquiles <aquiles.viza@sansano.usm.cl>
2020-05-22 18:17:16 -0400

    tb_fib_rec funciona. Hay que corregir tb_BCD_to_sevenSeg

--- sim/tb_BCD_to_sevenSeg.sv
+++ sim/tb_BCD_to_sevenSeg.sv
@@ -1,70 +1,71 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 15.05.2020 15:00:00
-// Design Name: 
+// Design Name:
 // Module Name: tb_fib_rec
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module tb_fib_rec();
-    /* 
+module tb_BCD_to_sevenSeg();
+    /*
         Cosas que tienes que corregir
         - El largo del vector de tests
         - La cantidad de bits de cada test (IN + OUT)
         - Si la salida es un vector, corrige el tipo de dato de 'expected'.
         - En el canto de subida, el ajuste de las entradas
         - El periodo del reloj, recuerda que para el canto de bajada la salida ya debe estar estable
-        - 
+        -
     */
-    
+
     parameter testvector_length = 20;
-    parameter testvector_name = "fib_rec.mem";
-    parameter testvector_bits = 5;
-    
-    // Variables del simulador    
+    parameter testvector_name = "BCD_to_sevenSeg.mem";
+    parameter testvector_bits = 11;
+
+    // Variables del simulador
     logic                       clk, reset;
-    logic                       expected;
+    logic [6:0]                 expected;
     logic [31:0]                vectornum, errors;
     logic [testvector_bits-1:0] testvector [testvector_length-1:0];
 
     // Variables del DUT
     logic [3:0]  BCD_in;
-    logic        y;
-    
-    fib_rec DUT(BCD_in, y);
+    logic [6:0]  y;
+
+    fib_rec DUT(
+            .BCD_in(BCD_in), .sevenSeg(y));
 
     // Reloj, ajusta el periodo
     always begin
         clk = ~clk;
         #5;
     end
-    
+
     // Obtención de datos, revisa instrucciones.md para ver como agregar el archivo al workspace
     initial begin
-        
+
         $readmemb(testvector_name, testvector);
         vectornum = 0;
         errors = 0;
         reset = 1;
         clk = 0;
-        
+
         #10;
         reset = 0;
-        
+
     end
 
     // Canto Subida -> Actualizar entrada
@@ -73,20 +74,20 @@ module tb_fib_rec();
         #1;
         {BCD_in, expected} = testvector[vectornum];
     end
-    
+
     // Canto Bajada -> Revisar salida
     // Esto debería ser editado para ajustarse a los otros módulos, pero perfectamente puede quitarse.
     always @(negedge clk) begin
         if (~reset) begin
             $display("%t: BCD_in = %d, y = %b, expected = %b", $realtime, BCD_in, y, expected);
-            
+
             if (y !== expected) begin
                 $error("Error: Cuando BCD_in = %d, y = %b y no %b", BCD_in, y, expected);
                 errors = errors + 1;
             end
-            
+
             vectornum = vectornum + 1;
-            
+
             if (testvector[vectornum] === {testvector_bits{1'bx}}) begin
                 $display("La verificación culmina con");
                 $display("%d tests y",vectornum);
